d57cbb3021fa3706ef556a5984b81f12f4fcfaba
fzerorubigd <fzerorubigd@gmail.com>
2013-01-14 20:03:32 +0330

    Initial project structure
    Signed-off-by: fzerorubigd <fzerorubigd@gmail.com>

---
+++ README.md
@@ -0,0 +1,18 @@
+Xamin UMS-WEB
+=============
+
+Installation
+============
+First install virtualenv, any way you like it. then :
+
+Create new virtual env :
+    ```virtualenv venv```
+
+Start your env :
+    ```. venv/bin/activate```
+
+Install requirement (for development): 
+    ```python ./setup.py develop```
+      
+Install :
+    ```python ./setup.py install```
---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+""" Setup" for pip
+"""
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import os
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+long_description = f.read()
+f.close()
+
+
+setup(
+    name='Ums2-web',
+    version='0.2.0',
+    author='fzerorubigd',
+    author_email='fzerorubigd@gmail.com',
+    packages=['ums-web'],
+    url='http://xamin.ir',
+    license='LICENSE.md',
+    description='Upstream management system for Xamin, web UI',
+    long_description=long_description,
+    install_requires=[
+        "redis>=2.7.0",
+        "Flask"
+    ],
+)
