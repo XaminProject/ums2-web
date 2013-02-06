9eecbeed98e55efdf5ce3ac9c82235c896448b3a
fzerorubigd <fzerorubigd@gmail.com>
2013-02-06 20:54:36 +0330

    more action
    Signed-off-by: fzerorubigd <fzerorubigd@gmail.com>

--- umsw/views/index.py
+++ umsw/views/index.py
@@ -5,7 +5,8 @@ from datetime import datetime
 from flask import Module, request, url_for, \
     render_template
 from redis import *
-from umsw.defaults import pool, REDIS_PREFIX, ADDED_LIST
+from umsw.defaults import pool, REDIS_PREFIX, ADDED_LIST, \
+    DL_LIST, ADDED_POSTFIX, BUILD_LIST, SLAVE_PREFIX
 from json import dumps, loads
 
 index = Module(__name__)
@@ -39,5 +40,47 @@ def show_added(page=1):
     count = 0;
     added = {}
     for pkg in all_data:
-        added[pkg] = pkgdata = r.hgetall(pkg)
+        added[pkg] = r.hgetall(pkg)
     return render_template('added.html', data=added)
+
+@index.route('/list-dl')
+def download_list():
+
+    r = Redis(connection_pool=pool)
+    all_data = r.lrange(DL_LIST, 0, r.llen(DL_LIST) + 1)
+    count = 0;
+    added = {}
+    for pkg in all_data:
+        x = -len(ADDED_POSTFIX)
+        pkg = pkg[:x]
+        added[pkg] = r.hgetall(pkg)
+    return render_template('listdl.html', data=added)
+
+
+@index.route('/list-build')
+def build_list():
+
+    r = Redis(connection_pool=pool)
+    all_data = r.lrange(BUILD_LIST, 0, r.llen(BUILD_LIST) + 1)
+    added = {}
+    x = -len(ADDED_POSTFIX)
+    for pkg in all_data:
+        pkg = pkg[:x]
+        added[pkg] = r.hgetall(pkg)
+    return render_template('listbuild.html', data=added)
+
+
+@index.route('/slaves')
+def slave_list():
+
+    r = Redis(connection_pool=pool)
+    all_data = r.keys(SLAVE_PREFIX + '*')
+
+    added = {}
+    x = -len(ADDED_POSTFIX)
+    for key in all_data:
+        pkg = r.get(key)
+        pkg = pkg[:x]
+        key = key[len(SLAVE_PREFIX):]
+        added[key] = r.hgetall(pkg)
+    return render_template('slaves.html', data=added)
